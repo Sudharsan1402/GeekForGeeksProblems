41313aea0724bf5386a247d16c1e4c253d6715e9
Sudharsan T <sudharsant2005@gmail.com>
2025-02-28 08:40:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Evaluation of Postfix Expression/evaluation-of-postfix-expression.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int evaluate(String[] arr) {
+        // code here
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : arr) {
+            
+            // If the token is a number, push it onto the stack
+            if (token.matches("-?\\d+")) {  
+                stack.push(Integer.parseInt(token));
+            } 
+            
+            // Otherwise, it must be an operator
+            else {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(val2 + val1);
+                        break;
+                    case "-":
+                        stack.push(val2 - val1);
+                        break;
+                    case "*":
+                        stack.push(val2 * val1);
+                        break;
+                    case "/":
+                        stack.push(val2 / val1);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+}
+
+
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int t = Integer.parseInt(line);
+        while (t-- > 0) {
+            line = reader.readLine();
+            String[] arr = line.split(" ");
+            Solution solution = new Solution();
+            System.out.println(solution.evaluate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
